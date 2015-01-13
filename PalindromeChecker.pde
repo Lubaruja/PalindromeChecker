5a23e93cb800495a3e24799169bb946821baef63
unknown <kijensen@697-334-Sauron.sf.k12.internal>
2015-01-13 11:11:48 -0800

    moo

--- PalindromeChecker.pde
+++ PalindromeChecker.pde
@@ -6,17 +6,33 @@ public void setup()
   {
     if(palindrome(lines[i])==true)
     {
-      println(lines[i] + " IS a palidrome.");
+      println(lines[i] + " IS a palindrome.");
     }
     else
     {
-      println(lines[i] + " is NOT a palidrome.");
+      println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  return false;
-}
 
+public boolean palindrome(String sWord)
+{
+  String usable = "";
+  for (int i=0; i<sWord.length(); i++)
+  {
+    if (Character.isLetter(sWord.charAt(i)))
+    {
+      usable+=sWord.charAt(i);
+    }
+  }
+  usable=usable.toLowerCase();
+  for (int k=0; k<usable.length(); k++)
+  {
+    if (usable.charAt(k)!=usable.charAt(usable.length()-k-1))
+    {
+      return false;
+    }
+  }
+  return true;
+
+}
--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -22,20 +22,38 @@ public void setup()
   {
     if(palindrome(lines[i])==true)
     {
-      println(lines[i] + " IS a palidrome.");
+      println(lines[i] + " IS a palindrome.");
     }
     else
     {
-      println(lines[i] + " is NOT a palidrome.");
+      println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
-public boolean palindrome(String word)
-{
-  //your code here
-  return false;
-}
 
+public boolean palindrome(String sWord)
+{
+  String usable = "";
+
+  for (int i=0; i<sWord.length(); i++)
+  {
+    if (Character.isLetter(sWord.charAt(i)))
+    {
+      usable+=sWord.charAt(i);
+    }
+  }
+  usable=usable.toLowerCase();
+  System.out.println("usable: "+usable);
+  for (int k=0; k<usable.length(); k++)
+  {
+    if (usable.charAt(k)!=usable.charAt(usable.length()-k-1))
+    {
+      return false;
+    }
+  }
+  return true;
+
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "PalindromeChecker" };
     if (passedArgs != null) {
